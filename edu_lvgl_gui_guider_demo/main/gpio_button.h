f906954660412b2a0471eba8cc8a768c4b387df7
XuBovey <xu.bovey@foxmail.com>
2021-09-09 19:43:33 +0800

    add edu_lvgl_gui_guider_demo

---
+++ edu_lvgl_gui_guider_demo/main/gpio_button.h
@@ -0,0 +1,14 @@
+/*
+ * gpio_button.h
+ *
+ *  Created on: 2021Äê7ÔÂ8ÈÕ
+ *      Author: Bovey
+ */
+
+#ifndef MAIN_GPIO_BUTTON_H_
+#define MAIN_GPIO_BUTTON_H_
+
+void gpio_button_init(void);
+bool get_gpio_state(uint8_t num);
+
+#endif /* MAIN_GPIO_BUTTON_H_ */
