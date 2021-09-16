aaf38ab9fedbf5e994581938ad5479332478e5a0
XuBovey <xu.bovey@foxmail.com>
2021-09-16 15:20:03 +0800

    增加支持时钟

--- edu_ledc/main/ledc_example_main.c
+++ edu_ledc/main/ledc_example_main.c
@@ -33,30 +33,19 @@
  *    GPIO4/5 are from low speed channel group.
  *
  */
-#if CONFIG_IDF_TARGET_ESP32
+
 #define LEDC_HS_TIMER          LEDC_TIMER_0
 #define LEDC_HS_MODE           LEDC_HIGH_SPEED_MODE
 #define LEDC_HS_CH0_GPIO       (21)
 #define LEDC_HS_CH0_CHANNEL    LEDC_CHANNEL_0
 #define LEDC_HS_CH1_GPIO       (22)
 #define LEDC_HS_CH1_CHANNEL    LEDC_CHANNEL_1
-#endif
-#define LEDC_LS_TIMER          LEDC_TIMER_1
-#define LEDC_LS_MODE           LEDC_LOW_SPEED_MODE
-#if CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32C3
-#define LEDC_LS_CH0_GPIO       (18)
-#define LEDC_LS_CH0_CHANNEL    LEDC_CHANNEL_0
-#define LEDC_LS_CH1_GPIO       (19)
-#define LEDC_LS_CH1_CHANNEL    LEDC_CHANNEL_1
-#endif
-#define LEDC_LS_CH2_GPIO       (23)
-#define LEDC_LS_CH2_CHANNEL    LEDC_CHANNEL_2
-//#define LEDC_LS_CH3_GPIO       (5)
-//#define LEDC_LS_CH3_CHANNEL    LEDC_CHANNEL_3
+#define LEDC_HS_CH2_GPIO       (23)
+#define LEDC_HS_CH2_CHANNEL    LEDC_CHANNEL_2
 
 #define LEDC_TEST_CH_NUM       (3)
-#define LEDC_TEST_DUTY         (8192)
-#define LEDC_TEST_FADE_TIME    (1000)
+#define LEDC_TEST_DUTY         (8191)
+#define LEDC_TEST_FADE_TIME    (3000)
 
 void app_main(void)
 {
@@ -69,18 +58,13 @@ void app_main(void)
     ledc_timer_config_t ledc_timer = {
         .duty_resolution = LEDC_TIMER_13_BIT, // resolution of PWM duty
         .freq_hz = 5000,                      // frequency of PWM signal
-        .speed_mode = LEDC_LS_MODE,           // timer mode
-        .timer_num = LEDC_LS_TIMER,            // timer index
+        .speed_mode = LEDC_HS_MODE,           // timer mode
+        .timer_num = LEDC_HS_TIMER,            // timer index
         .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
     };
     // Set configuration of timer0 for high speed channels
     ledc_timer_config(&ledc_timer);
-#ifdef CONFIG_IDF_TARGET_ESP32
-    // Prepare and set configuration of timer1 for low speed channels
-    ledc_timer.speed_mode = LEDC_HS_MODE;
-    ledc_timer.timer_num = LEDC_HS_TIMER;
-    ledc_timer_config(&ledc_timer);
-#endif
+
     /*
      * Prepare individual configuration
      * for each channel of LED Controller
@@ -95,10 +79,9 @@ void app_main(void)
      *         will be the same
      */
     ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
-#if CONFIG_IDF_TARGET_ESP32
         {
             .channel    = LEDC_HS_CH0_CHANNEL,
-            .duty       = 0,
+            .duty       = LEDC_TEST_DUTY,
             .gpio_num   = LEDC_HS_CH0_GPIO,
             .speed_mode = LEDC_HS_MODE,
             .hpoint     = 0,
@@ -106,46 +89,20 @@ void app_main(void)
         },
         {
             .channel    = LEDC_HS_CH1_CHANNEL,
-            .duty       = 0,
+            .duty       = LEDC_TEST_DUTY,
             .gpio_num   = LEDC_HS_CH1_GPIO,
             .speed_mode = LEDC_HS_MODE,
             .hpoint     = 0,
             .timer_sel  = LEDC_HS_TIMER
         },
-#elif CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32C3
         {
-            .channel    = LEDC_LS_CH0_CHANNEL,
-            .duty       = 0,
-            .gpio_num   = LEDC_LS_CH0_GPIO,
-            .speed_mode = LEDC_LS_MODE,
+            .channel    = LEDC_HS_CH2_CHANNEL,
+            .duty       = LEDC_TEST_DUTY,
+            .gpio_num   = LEDC_HS_CH2_GPIO,
+            .speed_mode = LEDC_HS_MODE,
             .hpoint     = 0,
-            .timer_sel  = LEDC_LS_TIMER
+            .timer_sel  = LEDC_HS_TIMER
         },
-        {
-            .channel    = LEDC_LS_CH1_CHANNEL,
-            .duty       = 0,
-            .gpio_num   = LEDC_LS_CH1_GPIO,
-            .speed_mode = LEDC_LS_MODE,
-            .hpoint     = 0,
-            .timer_sel  = LEDC_LS_TIMER
-        },
-#endif
-        {
-            .channel    = LEDC_LS_CH2_CHANNEL,
-            .duty       = 0,
-            .gpio_num   = LEDC_LS_CH2_GPIO,
-            .speed_mode = LEDC_LS_MODE,
-            .hpoint     = 0,
-            .timer_sel  = LEDC_LS_TIMER
-        },
-//        {
-//            .channel    = LEDC_LS_CH3_CHANNEL,
-//            .duty       = 0,
-//            .gpio_num   = LEDC_LS_CH3_GPIO,
-//            .speed_mode = LEDC_LS_MODE,
-//            .hpoint     = 0,
-//            .timer_sel  = LEDC_LS_TIMER
-//        },
     };
 
     // Set LED Controller with previously prepared configuration
@@ -156,26 +113,19 @@ void app_main(void)
     // Initialize fade service.
     ledc_fade_func_install(0);
 
-	for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
-		ledc_set_duty(ledc_channel[ch].speed_mode, ledc_channel[ch].channel, LEDC_TEST_DUTY);
-		ledc_update_duty(ledc_channel[ch].speed_mode, ledc_channel[ch].channel);
-	}
-	vTaskDelay(1000 / portTICK_PERIOD_MS);
-
     while (1) {
-        printf("1. LEDC fade up to duty = %d\n", LEDC_TEST_DUTY);
         for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
             ledc_set_fade_with_time(ledc_channel[ch].speed_mode,
-					ledc_channel[ch].channel, 0, LEDC_TEST_FADE_TIME);
-			ledc_fade_start(ledc_channel[ch].speed_mode,
-					ledc_channel[ch].channel, LEDC_FADE_NO_WAIT);
-			vTaskDelay(LEDC_TEST_FADE_TIME / portTICK_PERIOD_MS);
-
+                    ledc_channel[ch].channel, 0, LEDC_TEST_FADE_TIME);
+            ledc_fade_start(ledc_channel[ch].speed_mode,
+                    ledc_channel[ch].channel, LEDC_FADE_NO_WAIT);
+            vTaskDelay(LEDC_TEST_FADE_TIME / portTICK_PERIOD_MS);
             ledc_set_fade_with_time(ledc_channel[ch].speed_mode,
                     ledc_channel[ch].channel, LEDC_TEST_DUTY, LEDC_TEST_FADE_TIME);
             ledc_fade_start(ledc_channel[ch].speed_mode,
                     ledc_channel[ch].channel, LEDC_FADE_NO_WAIT);
             vTaskDelay(LEDC_TEST_FADE_TIME / portTICK_PERIOD_MS);
         }
+
     }
 }
